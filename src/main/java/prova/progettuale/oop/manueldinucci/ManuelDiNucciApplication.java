addf1dd9c8cfd23be4e8fc47cbcb16e6610ac3c4
Manuel <Manuel@LAPTOP-O7ECH6MO.station>
2019-06-19 12:12:49 +0200

    commenti iniziali

--- src/main/java/prova/progettuale/oop/manueldinucci/ManuelDiNucciApplication.java
+++ src/main/java/prova/progettuale/oop/manueldinucci/ManuelDiNucciApplication.java
@@ -1,13 +1,13 @@
-package prova.progettuale.oop.manueldinucci;
+package prova.progettuale.oop.manueldinucci; 
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication; //Import iniziali
+import org.springframework.boot.autoconfigure.SpringBootApplication; //Import iniziali
 
-@SpringBootApplication
+@SpringBootApplication //Commento iniziale
 public class ManuelDiNucciApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManuelDiNucciApplication.class, args);
+		SpringApplication.run(ManuelDiNucciApplication.class, args); //Riga iniziale
 	}
 
 }
