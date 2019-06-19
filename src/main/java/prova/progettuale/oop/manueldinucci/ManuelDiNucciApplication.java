edd95364bf59c66369f9c8cc5f32b1871158a316
Manuel <Manuel@LAPTOP-O7ECH6MO.station>
2019-06-19 11:30:02 +0200

    Commit iniziale

---
+++ src/main/java/prova/progettuale/oop/manueldinucci/ManuelDiNucciApplication.java
@@ -0,0 +1,13 @@
+package prova.progettuale.oop.manueldinucci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuelDiNucciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManuelDiNucciApplication.class, args);
+	}
+
+}
